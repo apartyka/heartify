195122d072401be6ce7b583506019429499bc4e3
apartyka <adampartyka@gmail.com>
2011-10-12 10:57:09 -0400

    ran heartify.js through beautifier

--- js/heartify.js
+++ js/heartify.js
@@ -12,44 +12,44 @@
  	Check the readme file on github (github.com/apartyka/heartify) for additional notes and implementation.
 */
 
-(function ($) {
+(function($) {
 
-   $.fn.heartify = function (options) {
+    $.fn.heartify = function(options) {
 
-      var defaults = {
+        var defaults = {
 
-         labelText: 'What do you heart?',
-         textArea: { 
-        	 value: 'I heart'
-         },
-         cssClass: {
-            enabled: 'enabled',
-            disabled: 'disabled'
-         },
-         attr: {
-            enabled: 'enabled',
-            disabled: 'disabled'
-         },
-         counter: 133,
-         on_submit : null
-         
-      };
+            labelText: 'What do you heart?',
+            textArea: {
+                value: 'I heart'
+            },
+            cssClass: {
+                enabled: 'enabled',
+                disabled: 'disabled'
+            },
+            attr: {
+                enabled: 'enabled',
+                disabled: 'disabled'
+            },
+            counter: 133,
+            on_submit: null
 
-      options = $.extend({}, defaults, options);
+        };
 
-      return this.each(function () {
+        options = $.extend({}, defaults, options);
 
-         //form objects
-         var $formDiv = $('<div></div>'),
+        return this.each(function() {
+
+            //form objects
+            var $formDiv = $('<div></div>'),
             $labelText = $('<p></p>'),
             $textArea = $('<textarea></textarea>'),
             $counter = $('<span></span>'),
             $submitBtn = $('<input type="submit" value="Submit" />');
-         
-         //object we're enabling the plugin on
-         var $anchor = $(this);
 
-         /*
+            //object we're enabling the plugin on
+            var $anchor = $(this);
+
+            /*
             Initial click function:
               - click $anchor element
               - set $labelText's html to our default labelText
@@ -60,27 +60,27 @@
               - set focus to textarea
               - set $submitBtn attr to disabled, addClass disabled
          */
-         $anchor.click(function (e) {
+            $anchor.click(function(e) {
 
-            $labelText.html(options.labelText);
+                $labelText.html(options.labelText);
 
-            $formDiv.appendTo($anchor.parent());
+                $formDiv.appendTo($anchor.parent());
 
-            $anchor.hide();
-            
-            $formDiv.append($labelText, $textArea, $counter, $submitBtn).fadeIn();
+                $anchor.hide();
 
-            $counter.html(options.counter);
+                $formDiv.append($labelText, $textArea, $counter, $submitBtn).fadeIn();
 
-            $textArea.val(options.textArea.value);
+                $counter.html(options.counter);
 
-            $submitBtn.attr('disabled', options.attr.disabled).addClass(options.cssClass.disabled);
+                $textArea.val(options.textArea.value);
 
-            e.preventDefault();
+                $submitBtn.attr('disabled', options.attr.disabled).addClass(options.cssClass.disabled);
 
-         });
+                e.preventDefault();
 
-         /*
+            });
+
+            /*
             Keyup function:
              - If count is equal to 140 or less than 0 - remove enabled attr, add disabled attr, remove enabled class, add disabled class
              Else, remove disabled attr, add enabled attr, remove disabled class, add enabled class
@@ -88,72 +88,71 @@
              Else remove disabled class
              - Subtracts from $counter w/ each character keyup
          */
-         $textArea.keyup(function () {
-        	 
-             var count = 140 - $textArea.val().length;
+            $textArea.keyup(function() {
 
-             if (count === 140 || count < 0) {
-                $submitBtn.removeAttr(options.attr.enabled)
-                .attr('disabled', options.attr.disabled)
-                .removeClass(options.cssClass.enabled)
-                .addClass(options.cssClass.disabled);
-             } else {
-                $submitBtn.removeAttr(options.attr.disabled)
-                .attr('enabled', options.attr.enabled)
-                .removeClass(options.cssClass.disabled)
-                .addClass(options.cssClass.enabled);
-             }
+                var count = 140 - $textArea.val().length;
 
-             if (count < 0) {
-                $counter.addClass(options.cssClass.disabled);
-             } else {
-                $counter.removeClass(options.cssClass.disabled);
-             }
+                if (count === 140 || count < 0) {
+                    $submitBtn.removeAttr(options.attr.enabled)
+                    .attr('disabled', options.attr.disabled)
+                    .removeClass(options.cssClass.enabled)
+                    .addClass(options.cssClass.disabled);
+                } else {
+                    $submitBtn.removeAttr(options.attr.disabled)
+                    .attr('enabled', options.attr.enabled)
+                    .removeClass(options.cssClass.disabled)
+                    .addClass(options.cssClass.enabled);
+                }
 
-             $counter.html(count);
-        	 
-         });
-         
-         /*
-         
+                if (count < 0) {
+                    $counter.addClass(options.cssClass.disabled);
+                } else {
+                    $counter.removeClass(options.cssClass.disabled);
+                }
+
+                $counter.html(count);
+
+            });
+
+            /*
+
          Submit function:
            - ajax call to submit the textArea.val() to a results <div>
            - detach form elements, restore $anchor to its original visual state
-          
+
          */
-         $submitBtn.click(function(e) {
-       	  
-	       	 $.ajax({ 
-	       		 type: 'POST',
-	       		 url: 'index.html',
-	       		 datatype: 'html',
-	       		 context : $submitBtn,
-	       		 success: function() { 
-	       			
-        			 var data = $textArea.val();
-        			 //find $submitBtn's parent, append data to a <p> within the sibling .results <div>
-        			 $(this).parent().parent().siblings('.results').append(	
-        				$('<p>' + data + '</p>')
-        			 );
-	       			 
-	       			$formDiv.detach().css( { 'display' : 'none' } );
-	           		 
-	       			$anchor.fadeIn();
-	       			
-	       			if (typeof options.on_submit == 'function') {
-	       				options.on_submit();
-	       			}
-	       			
-	       		 }
-	       	 });
-	       	  
-	       	 e.preventDefault();
-	       	 
-         });
+            $submitBtn.click(function(e) {
 
-      });
+                $.ajax({
+                    type: 'POST',
+                    url: 'index.html',
+                    datatype: 'html',
+                    context: $submitBtn,
+                    success: function() {
 
-   };
+                        var data = $textArea.val();
+                        //find $submitBtn's parent, append data to a <p> within the sibling .results <div>
+                        $(this).parent().parent().siblings('.results').append($('<p>' + data + '</p>'));
 
+                        $formDiv.detach().css({
+                            'display': 'none'
+                        });
+
+                        $anchor.fadeIn();
+
+                        if (typeof options.on_submit == 'function') {
+                            options.on_submit();
+                        }
+
+                    }
+                });
+
+                e.preventDefault();
+
+            });
+
+        });
+
+    };
 
 })(jQuery);
